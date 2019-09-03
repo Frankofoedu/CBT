4818dcdf02caa305af31c118600b99baa68e9dcd
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-09-03 16:13:43 +0100

    Pdf certificate generator

---
+++ PDFCertificateGenerator/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace PDFCertificateGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(98, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Student Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(326, 214);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(173, 61);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "GenerateCertificates";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Test Cert Generator";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(204, 37);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(197, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(204, 63);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(197, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Full Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 296);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PDFCertificateGenerator/Form1.cs
@@ -0,0 +1,184 @@
+﻿using iTextSharp.text.pdf;
+using Newtonsoft.Json;
+using rating.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFCertificateGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private readonly HttpClient client = new HttpClient();
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+                var response = await client.GetAsync("http://cbt.insyt.com.ng/api/Users");//, new StringContent(str, Encoding.UTF8, "application/json"));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonObject = await response.Content.ReadAsStringAsync();
+
+                    var users = JsonConvert.DeserializeObject<List<User>>(jsonObject);
+                    if (users.Count < 1)
+                    {
+                        MessageBox.Show("No users registered");
+                        return;
+                    }
+
+                    File.WriteAllText(Constants.allUserPath, jsonObject);
+
+
+                    MessageBox.Show("User data has been collected");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+        }
+
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(@"Users.json"))
+            {
+                var user_file = File.ReadAllText(@"Users.json");
+
+                var jsonData = JsonConvert.DeserializeObject<List<User>>(user_file);
+
+                if (jsonData != null)
+                {
+                    var i = 0;
+                    Cursor = Cursors.WaitCursor;
+                    foreach (var user in jsonData)
+                    {
+                        try
+                        {
+                            FillForm(user);
+                            i++;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                    Cursor = Cursors.Default;
+                    MessageBox.Show($"{i} certificates generated");
+                    return;
+                }
+
+                MessageBox.Show("An error occured while parsing data. contact ebuka");
+                return;
+               // return jsonData;
+            }
+            MessageBox.Show("File does not exist");
+           // return null;
+        }
+
+
+        private void FillForm(User user)
+        {
+            string pdfTemplate = @"fill3.pdf";
+
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Certificate");
+
+            Directory.CreateDirectory(path);
+
+
+
+            string newFile = Path.Combine( path, user.Email + ".pdf") ; 
+
+            var pdfReader = new PdfReader(pdfTemplate);
+            var pdfStamper = new PdfStamper(pdfReader, new FileStream(
+                newFile, FileMode.Create));
+            var pdfFormFields = pdfStamper.AcroFields;
+
+            var fontpath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), @"fonts\TypographerRotunda.ttf");
+            var fontCustom = BaseFont.CreateFont(fontpath, BaseFont.CP1252, BaseFont.EMBEDDED);
+            var font = new iTextSharp.text.Font(fontCustom, 12f);
+
+
+            // pdfFormFields.SetFieldProperty("name", "textsize", 16f, null);
+            // var s = FontFactory.GetFont(FontFactory.COURIER_BOLD, 2f, iTextSharp.text.Font.BOLD);
+            //pdfFormFields.AddSubstitutionFont(s.BaseFont);
+            pdfFormFields.SetFieldProperty("name", "textfont", font.BaseFont, null);
+            pdfFormFields.SetField("name", user.FullName.ToUpper());
+
+
+            pdfFormFields.SetField("serialNo", user.Id.ToString("00000"));
+
+
+            pdfStamper.FormFlattening = true;
+
+            // close the pdf
+
+            pdfStamper.Close();
+            //throw new NotImplementedException();
+        }
+        private void FillForm(string FullName, string Email, string sNo)
+        {
+            string pdfTemplate = @"fill3.pdf";
+
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Certificate");
+            Directory.CreateDirectory(path);
+
+
+
+            string newFile = Path.Combine(path, Email + ".pdf");
+
+            var pdfReader = new PdfReader(pdfTemplate);
+            var pdfStamper = new PdfStamper(pdfReader, new FileStream(
+                newFile, FileMode.Create));
+            var pdfFormFields = pdfStamper.AcroFields;
+
+            var fontpath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), @"fonts\TypographerRotunda.ttf");
+            var fontCustom = BaseFont.CreateFont(fontpath, BaseFont.CP1252, BaseFont.EMBEDDED);
+            var font = new iTextSharp.text.Font(fontCustom, 12f);
+
+
+            // pdfFormFields.SetFieldProperty("name", "textsize", 16f, null);
+            // var s = FontFactory.GetFont(FontFactory.COURIER_BOLD, 2f, iTextSharp.text.Font.BOLD);
+            //pdfFormFields.AddSubstitutionFont(s.BaseFont);
+            pdfFormFields.SetFieldProperty("name", "textfont", font.BaseFont, null);
+            pdfFormFields.SetField("name", FullName);
+
+
+            pdfFormFields.SetField("serialNo", sNo);
+
+
+            pdfStamper.FormFlattening = true;
+
+            // close the pdf
+
+            pdfStamper.Close();
+            //throw new NotImplementedException();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            FillForm(txtName.Text, txtEmail.Text, "001");
+            Cursor = Cursors.Default;
+
+            MessageBox.Show("Generated");
+        }
+    }
+}
